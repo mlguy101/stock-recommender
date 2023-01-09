f7537626daa2ca291198f13b47df2e828bfcbe1e
mbaddar <mbaddar2@gmail.com>
2023-01-09 14:54:11 +0100

    started code for local minima/ maxima finding for generating target signals (buy / sell / hold ) "

---
+++ ml/y_gen.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from talipp.indicators import EMA
+import plotly.express as px
+
+
+def generate_y(df: pd.DataFrame, ma_window=5, colname='Adj Close'):
+    """
+
+    :param df:
+    :param ma_window:
+    :param colname:
+    :return:
+    """
+    # TODO
+    """
+    https://eddwardo.github.io/posts/2019-06-05-finding-local-extreams-in-pandas-time-series/
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks_cwt.html#scipy.signal.find_peaks_cwt 
+    """
+
+    df['ema'] = [np.nan] * (ma_window - 1) + EMA(period=ma_window, input_values=df[colname]).output_values
+    # plt.plot(df.index, df['ema'])
+    fig = px.line(data_frame=df, x=df.index, y='ema')
+    fig.show()
+    return df
